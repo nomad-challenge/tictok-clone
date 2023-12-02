2e4db4da22353a3d1944c00af5475e7f9228deff
antnf3 <antnf3@gmail.com>
2023-12-02 13:20:18 +0900

    discover - gridItem, FadeInImage, AspectRatio

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -54,10 +54,15 @@ class DiscoverScreen extends StatelessWidget {
                 childAspectRatio: 9 / 16,
               ),
               itemBuilder: (context, index) {
-                return FadeInImage.assetNetwork(
-                  placeholder: "assets/images/tictok_placeholder.png",
-                  image:
-                      "https://images.unsplash.com/photo-1701114413455-875c598cd407?q=80&w=2959&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                return AspectRatio(
+                  aspectRatio: 9 / 16,
+                  child: FadeInImage.assetNetwork(
+                    fit: BoxFit.cover,
+                    placeholderFit: BoxFit.cover,
+                    placeholder: "assets/images/tictok_placeholder.png",
+                    image:
+                        "https://images.unsplash.com/photo-1701114413455-875c598cd407?q=80&w=2959&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  ),
                 );
               }),
           for (var tab in tabs.skip(1))
