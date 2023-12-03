2c63c306eee8850d5e1889b020287f34d65e2a3b
antnf3 <antnf3@gmail.com>
2023-12-03 16:29:37 +0900

    10.InBox listTile

--- lib/features/inbox/inbox_screen.dart
+++ lib/features/inbox/inbox_screen.dart
@@ -1,14 +1,83 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mynote/constants/sizes.dart';
 
 class InboxScreen extends StatelessWidget {
   const InboxScreen({super.key});
+
+  void _onDmPressed() {
+    print("send DM");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Inbox"),
+        elevation: 1,
+        actions: [
+          IconButton(
+            onPressed: _onDmPressed,
+            icon: const FaIcon(FontAwesomeIcons.paperPlane),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () {},
+            title: const Text(
+              "Activity",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+            ),
+            trailing: const FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size16,
+              color: Colors.black,
+            ),
+          ),
+          Container(
+            height: Sizes.size1,
+            color: Colors.grey.shade200,
+          ),
+          ListTile(
+            leading: Container(
+              width: Sizes.size52,
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+              child: const Center(
+                child: FaIcon(
+                  FontAwesomeIcons.users,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            title: const Text(
+              "New followers",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: const Text(
+              "Messages from followers will appear here",
+              style: TextStyle(
+                fontSize: Sizes.size14,
+              ),
+            ),
+            trailing: const FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size16,
+              color: Colors.black,
+            ),
+          )
+        ],
       ),
     );
   }
